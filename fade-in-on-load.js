d300e1bbc3dccb2adaa619e3948e09168f3809de
yossefahmed6332 <yossefahmed6332@gmail.com>
2024-08-30 23:15:53 +0000

    Add files via upload

---
+++ fade-in-on-load.js
@@ -0,0 +1,6 @@
+// Fade-In Animation
+window.addEventListener('load', () => {
+    document.body.style.opacity = '0';
+    document.body.style.transition = 'opacity 1s';
+    document.body.style.opacity = '1';
+});
