b7128b18eb5b3112c322cfd4fdd4112c877c6490
yossefahmed6332 <yossefahmed6332@gmail.com>
2024-08-31 15:03:17 +0000

    Delete fade-in-on-load.js

--- fade-in-on-load.js
+++
@@ -1,6 +0,0 @@
-// Fade-In Animation
-window.addEventListener('load', () => {
-    document.body.style.opacity = '0';
-    document.body.style.transition = 'opacity 1s';
-    document.body.style.opacity = '1';
-});
